c242ab5dd029538932a1661ef742259686be1f59
Reshetnikov Roman <romanokd@yandex.ru>
2020-04-17 02:00:21 +0300

    CSharp Linear

--- LinearEquationTest/UnitTest1.cs
+++ LinearEquationTest/UnitTest1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task2;
 
 namespace LinearEquationTest
 {
@@ -7,8 +9,145 @@ namespace LinearEquationTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Array()
         {
+            const int n = 5;
+            double[] coeff = new double[n] { 1, 2, 3, 4, 5 };
+            LinearEquation a1 = new LinearEquation(coeff);
+            Assert.AreEqual(2, a1[1]);
+        }
+
+        [TestMethod]
+        public void List()
+        {
+            List<double> coeff = new List<double>() { 1, 2, 3, 4, 5 };
+            LinearEquation a1 = new LinearEquation(coeff);
+            Assert.AreEqual(2, a1[1]);
+        }
+
+        [TestMethod]
+        public void IndexEl()
+        {
+            string coeff = "1,2,3,4,5,6,7";
+            LinearEquation a1 = new LinearEquation(coeff);
+            Assert.AreEqual(3, a1[2]);
+        }
+
+        [TestMethod]
+        public void Sum()
+        {
+            string coeff1 = "1,3,5,7,9,12,15";
+            LinearEquation a1 = new LinearEquation(coeff1);
+            string coeff2 = "1,2,3,4,5,6,7";
+            LinearEquation a2 = new LinearEquation(coeff2);
+            string res = "2,5,8,11,14,18,22";
+            Assert.AreEqual(new LinearEquation(res), a1 + a2);
+        }
+
+        [TestMethod]
+        public void Sub()
+        {
+            string coeff1 = "2,3,4,5,6";
+            LinearEquation a1 = new LinearEquation(coeff1);
+            string coeff2 = "1,2,3,4,5";
+            LinearEquation a2 = new LinearEquation(coeff2);
+            string res = "1,1,1,1.1,1";
+            Assert.AreEqual(new LinearEquation(res), a1 - a2);
+        }
+
+        [TestMethod]
+        public void Mul1()
+        {
+            string coeff = "1,2,3,4,5";
+            LinearEquation a = new LinearEquation(coeff);
+            string res = "2,4,6,8,10";
+            Assert.AreEqual(new LinearEquation(res), a * 2);
+        }
+
+        [TestMethod]
+        public void Mul2()
+        {
+            string coeff = "1,2,3,4,5";
+            LinearEquation a = new LinearEquation(coeff);
+            string res = "2,4,6,8,10";
+            Assert.AreEqual(new LinearEquation(res), 2 * a);
+        }
+   
+        [TestMethod]
+        public void Minus()
+        {
+            string coeff = "1,3,5,7,9";
+            LinearEquation a = new LinearEquation(coeff);
+            string res = "-1,-3,-5,-7,-9";
+            Assert.AreEqual(new LinearEquation(res), -a);
+        }
+
+        [TestMethod]
+        public void Mass()
+        {
+            LinearEquation a = new LinearEquation(4);
+            a.MassArr(1.2);
+            string res = "1,2,3,4,5";
+            Assert.AreEqual(new LinearEquation(res), a);
+        }
+
+        [TestMethod]
+        public void Zero()
+        {
+            int n = 5;
+            LinearEquation a = new LinearEquation(n);
+            Assert.AreEqual("0x1+0x2+0x3+0x4+0x5=0", a.ToString());
+        }
+
+        [TestMethod]
+        public void Index()
+        {
+            string coeff = "1,2,3,7,8";
+            LinearEquation a = new LinearEquation(coeff);
+            Assert.AreEqual(3, a[2]);
+        }
+
+        [TestMethod]
+        public void BadEquation()
+        {
+            string res = "1.2,0,0,0";
+            LinearEquation a = new LinearEquation(res);
+            bool check = (a) ? true : false;
+            Assert.AreEqual(false, check);
+        }
+
+        [TestMethod]
+        public void TrueEquation()
+        {
+            string res = "1,4,0,0";
+            LinearEquation a = new LinearEquation(res);
+            bool check = (a) ? true : false;
+            Assert.AreEqual(true, check);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void BigIndex1()
+        {
+            string coeff = "1,2,3,5,6";
+            LinearEquation a = new LinearEquation(coeff);
+            Assert.Equals(typeof(IndexOutOfRangeException), a[18]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void MinusIndex2()
+        {
+            string coeff = "1,2,3,5,6";
+            LinearEquation a = new LinearEquation(coeff);
+            Assert.Equals(typeof(IndexOutOfRangeException), a[-18]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Argument()
+        {
+            Assert.Equals(typeof(ArgumentException), new LinearEquation(-50));
         }
     }
 }
---
+++ Task2/LinearEquation.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class LinearEquation
+    {
+        public List<double> indexes;
+        public LinearEquation(double[] cf)
+        {
+            indexes = new List<double>(cf);
+        }
+        public LinearEquation(List<double> cf)
+        {
+            indexes = new List<double>(cf);
+        }
+        public LinearEquation(int n)
+        {
+            if (n > 0)
+            {
+                indexes = new List<double>();
+                for (int i = 0; i <= n; i++)
+                    indexes.Add(0);
+            }
+            else throw new ArgumentException();
+        }
+        public int Size
+        {
+            get
+            {
+                return indexes.Count;
+            }
+        }
+        public LinearEquation(IEnumerable<double> cf)
+        {
+            indexes = new List<double>(cf);
+        }
+        public LinearEquation(string coeff)
+        {
+            string[] cf = Regex.Split(coeff, @"[^\d.-]");
+            indexes = new List<double>();
+            for (int i = 0; i < coeff.Length; i++)
+            {
+                if (cf[i] != "")
+                {
+                    cf[i] = cf[i].Replace('.', ',');
+                    indexes.Add(double.Parse(cf[i]));
+                }
+            }
+        }
+        public void Random()
+        {
+            Random rand = new Random();
+            for (int i = 0; i < Size; i++)
+                indexes[i] = rand.Next(1,100);
+        }
+        public void MassArr(double value)
+        {
+            for (int i = 0; i < Size; i++)
+                indexes[i] = value;
+        }
+        public double this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < Size)
+                    return indexes[index];
+                else throw new IndexOutOfRangeException();
+            }
+            set
+            {
+                if (index >= 0 && index < Size)
+                    indexes[index] = value;
+                else throw new IndexOutOfRangeException();
+            }
+        }
+
+        public static LinearEquation operator +(LinearEquation a, LinearEquation b)
+        {
+            var result = a.indexes.Zip(b.indexes, (first, second) => first + second);
+            return new LinearEquation(result);
+        }
+        public static LinearEquation operator -(LinearEquation a, LinearEquation b)
+        {
+            var result = a.indexes.Zip(b.indexes, (first, second) => first - second);
+            return new LinearEquation(result);
+        }
+        public static LinearEquation operator *(LinearEquation a, double r)
+        {
+            List<double> result = new List<double>();
+            for (int i = 0; i < a.Size; i++)
+                result.Add(a[i] * r);
+            return new LinearEquation(result);
+        }
+        public static LinearEquation operator *(double r, LinearEquation a)
+        {
+            return a * r;
+        }
+        public static LinearEquation operator -(LinearEquation a)
+        {
+            List<double> result = new List<double>();
+            for (int i = 0; i < a.Size; i++)
+                result.Add(-a[i]);
+            return new LinearEquation(result);
+        }
+        public override string ToString()
+        {
+            string res = "";
+            int i;
+            for (i = 0; i < Size - 2; i++)
+                res += (indexes[i + 1] >= 0) ? (indexes[i].ToString() + 'x' + (i + 1).ToString() + '+') : (indexes[i].ToString() + 'x' + (i + 1).ToString());
+            res += (indexes[i].ToString() + 'x' + (i + 1).ToString());
+            res += '=' + indexes[Size - 1].ToString();
+            return res;
+        }
+        public static bool operator ==(LinearEquation a, LinearEquation b)
+        {
+            return a.Equals(b);
+        }
+        public static bool operator !=(LinearEquation a, LinearEquation b)
+        {
+            return !(a == b);
+        }
+        public static implicit operator double[](LinearEquation a)
+        {
+            return a.indexes.ToArray();
+        }
+        public static bool operator false(LinearEquation a)
+        {
+            if (a)
+                return false;
+            else
+                return true;
+        }
+        public static bool operator true(LinearEquation a)
+        {
+            for (int i = 0; i < a.Size - 1; i++)
+                if (a[i] != 0)
+                    return true;
+            return (a[a.Size - 1] != 0) ? false : true;
+        }
+        public override bool Equals(object obj)
+        {
+            LinearEquation a = (LinearEquation)obj;
+            for (int i = 0; i < Size; i++)
+                if (Math.Abs(this[i] - a[i]) > 1e-9)
+                    return false;
+            return true;
+        }
+        public bool IsNull()
+        {
+            for (int i = 0; i < Size; i++)
+                if (this[i] != 0)
+                    return false;
+            return true;
+        }
+        public void CopyTo(LinearEquation a)
+        {
+            a.indexes = indexes.ToList();
+        }
+    }
+}
